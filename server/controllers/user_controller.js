afa5d3a5376ef56cfe946ffb5da62f6012cb017b
Queenie Ho <qho.queenieho@gmail.com>
2017-04-19 14:51:23 -0700

    login completed

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -5,15 +5,27 @@ var path = require("path");
 var htmlPath = path.join(__dirname, "./../../client/");
 var requireFolder = require("./../config/req_folder.js");
 var models = require(path.join(__dirname, "./../config/model_combiner.js"));
+var bcrypt = require("bcryptjs");
 
 //when you call a model function it should return a value (usually an array, the result of a query)
 //after that you can make the response here in the controller
 
 module.exports = {
 	userLogin: function(req, res){
-		models.login(req, res, function(err, rows, fields){
+		var validationErrors = [];
+		models.userLogin(Object.assign({sessionID: req.sessionID}, req.body), function(err, rows, fields){
+			console.log(rows, req.body, "!!!");
+			if (err || rows.length === 0){
+				validationErrors.push("Invalid login.");
+				res.json({errors: validationErrors, success: false  });
 
-			res.json({errors: err, data: rows});
+			}
+			else if (bcrypt.compareSync(req.body.password, rows[0].password)){
+				req.session.data = {id : rows[0].id, username : rows[0].username};
+				console.log("888888", bcrypt.compareSync(req.body.password, rows[0].password) );
+				res.json({errors: validationErrors, success: true });
+			}
+			
 		});
 	},
 	userRegistration: function(req, res){
@@ -39,12 +51,13 @@ module.exports = {
 			validationErrors.push("Invalid email.");
 		}
 
-		if(!/^[a-z ]{2,32}$/i.test(req.body.username)){
+		if(!/[A-Za-z0-9\_]+/.test(req.body.username)){
 			valid = false;
 			validationErrors.push("Invalid username. Only numbers and letters are allowed.");
 		}
 
 		if(valid){
+			req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
 			models.registration(Object.assign({sessionID: req.sessionID}, req.body), function(err, rows, fields){
 				if(err){
 					if(err.code === "ER_DUP_ENTRY"){
