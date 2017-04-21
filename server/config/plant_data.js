dc4abb226cbcc057680337a1e140832094c4366c
Queenie Ho <qho.queenieho@gmail.com>
2017-04-20 17:09:47 -0700

    trade plants routes

--- server/config/plant_data.js
+++ server/config/plant_data.js
@@ -3,21 +3,25 @@ module.exports = {
 		{
 			name: "Venus Flytrap",
 			description: "It eats flies.",
+			price: 5,
 			image_url: "https://s3-us-west-1.amazonaws.com/fluffy-system-plant-app/plant-images/venus_flytrap.jpg"
 		},
 		{
 			name: "Eggplant",
 			description: "It's purple.",
+			price: 2,
 			image_url: "https://s3-us-west-1.amazonaws.com/fluffy-system-plant-app/plant-images/eggplant.jpg"
 		},
 		{
 			name: "Watermelon",
 			description: "It's green with red inside.",
+			price: 1,
 			image_url: "https://s3-us-west-1.amazonaws.com/fluffy-system-plant-app/plant-images/watermelon.jpg"
 		},
 		{
 			name: "Zucchini",
 			description: "It's green and long.",
+			price: 10,
 			image_url: "https://s3-us-west-1.amazonaws.com/fluffy-system-plant-app/plant-images/zucchini.jpg"
 		}
 	]
--- server/controllers/market.js
+++ server/controllers/market.js
@@ -1,13 +1,19 @@
 //Market Controller
 
+
 var path = require("path");
 var htmlPath = path.join(__dirname, "./../../client/");
 var requireFolder = require("./../config/req_folder.js");
 var models = require(path.join(__dirname, "./../config/model_combiner.js"));
+var plants = require(path.join(__dirname, "./../config/plant_data.js"));
 
 module.exports = {
 	buyPlant: function(req, res){
-		models.buyPlant({id: req.session.data.id}, function(err, rows, fields){
+
+		models.buyPlant(Object.assign({id: req.session.data.id, plant_number: req.body.plant_number}, plants.plants[req.body.plant_number]), function(err, rows, fields){
+			console.log(err, "err from buyPlant control");
+			console.log(rows, "rows from buyPlant control");
+			// res.status((err)?500:200).json({errors: err, data: rows});
 			res.status((err)?500:200).json(err, rows);
 		});
 	}
