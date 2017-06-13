0fbc1cbf8778a6abfbb4d02e9d3fc2fd60796243
Chris Rollins <chris.rollins@leapfire.com>
2017-06-13 12:48:43 -0700

    changed a comment

--- server/models/dashboard_plants.js
+++ server/models/dashboard_plants.js
@@ -5,13 +5,13 @@ module.exports = {
 		doQuery("SELECT COUNT(*) FROM plants", function(err, count){
 			console.log(count);
 
-			if(count < 100) //this checks if the DB is big enough to start only pulling some of it.
+			if(count < 100) //if the DB is big we will pull the first 10 of a random sample
 			{
 				doQuery("SELECT * FROM plants", callback);
 			} else {
 				doQuery("SELECT * FROM plants WHERE (ABS(CAST((BINARY_CHECKSUM(*) * RAND()) as int)) % 100) < 10 LIMIT 100", callback);
 			}
-		})
+		});
 		
 	}
 }
